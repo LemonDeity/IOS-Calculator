3a8d57fbf8be3b13aa91a5fcb2684859a1a0c948
Noah R <nrange2001@gmail.com>
2019-10-25 11:38:44 -0500

    added method that is connected to all buttons and takes input

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -38,7 +38,35 @@ class ViewController: UIViewController {
     
    
     @IBAction func buttonPressed(_ sender: UIButton) {
-        
+        if let u = sender.titleLabel?.text{
+            switch(u){
+            case "0":
+                print(0)
+            case "1":
+                print(1)
+            case "2":
+                print(2)
+            case "3":
+                print(3)
+            case "4":
+                print(4)
+            case "5":
+                print(5)
+            case "7":
+                print(7)
+            case "8":
+                print(8)
+            case "9":
+                print(9)
+            case "+":
+                print("+")
+            case "-":
+                print("-")
+            
+            default:
+                print("Something isn't hooked up")
+            }
+        }
     }
     
 
