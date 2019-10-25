f293fa189a69bca6a58f6913d0f370691b63d115
Noah R <nrange2001@gmail.com>
2019-10-25 11:25:07 -0500

    made changes to UI can added all buttons to viewController

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answer: UILabel!
+    @IBOutlet weak var b0: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var b4: UIButton!
+    @IBOutlet weak var b5: UIButton!
+    @IBOutlet weak var b6: UIButton!
+    @IBOutlet weak var b7: UIButton!
+    @IBOutlet weak var b8: UIButton!
+    @IBOutlet weak var b9: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var flipSign: UIButton!
+    @IBOutlet weak var modulus: UIButton!
+    @IBOutlet weak var divide: UIButton!
+    @IBOutlet weak var multiply: UIButton!
+    @IBOutlet weak var minus: UIButton!
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var decimal: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        answer.text = ""
     }
-
+    
+    
+   
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
